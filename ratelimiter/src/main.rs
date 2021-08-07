53a0632ccdb0227c95d12c1a0893fd6f81ab7839
n1c00o <git.n1c00o@gmail.com>
2021-08-07 21:07:39 +0200

    Start gRPC implementation

--- ratelimiter/src/main.rs
+++ ratelimiter/src/main.rs
@@ -1,5 +1,52 @@
-// Some implementation of the gRPC service using the shared library.
+use std::net::SocketAddr;
+pub mod nova_ratelimit_v1 {
+    tonic::include_proto!("nova.ratelimit.v1");
+}
 
-fn main () {
-    println!("Hello world!")
+use nova_ratelimit_v1::{
+    ratelimit_response, ratelimit_service_client, ratelimit_service_server, CreateBucketData,
+    RatelimitRequest, RatelimitResponse,
+};
+
+#[derive(Debug, Default)]
+pub struct RatelimiterService {}
+
+#[tonic::async_trait]
+impl ratelimit_service_server::RatelimitService for RatelimiterService {
+    async fn create_bucket(
+        &self,
+        request: tonic::Request<nova_ratelimit_v1::CreateBucketData>,
+    ) -> Result<tonic::Response<nova_ratelimit_v1::CreateBucketData>, tonic::Status> {
+        // todo
+
+        Ok(tonic::Response::new(CreateBucketData {
+            limit: 0,
+            remaining: 0,
+            request: Some(RatelimitRequest {
+                indentifiables: Vec::<String>::new(),
+                route_name: String::from("todo"),
+            }),
+            reset: 0,
+        }))
+    }
+
+    async fn get_ratelimit_status(
+        &self,
+        request: tonic::Request<nova_ratelimit_v1::RatelimitRequest>,
+    ) -> Result<tonic::Response<nova_ratelimit_v1::RatelimitResponse>, tonic::Status> {
+        // todo
+
+        Ok(tonic::Response::new(RatelimitResponse {
+            status: ratelimit_response::Status::Ok as i32,
+            update_asked: false,
+        }))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr: SocketAddr = "[::0]:50051".parse()?; // Use ::0 and not ::1 because it does not work in Docker
+
+    println!("Ratelimiter service listening on {}", addr);
+    Ok(())
 }
