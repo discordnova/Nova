3aa8e88fec0d500e359bc2a2f355b6138b91c287
n1c00o <git.n1c00o@gmail.com>
2021-08-08 19:17:43 +0200

    Add Redis Client

--- ratelimiter/src/redis_client.rs
+++ ratelimiter/src/redis_client.rs
@@ -1,39 +1,97 @@
-use std::fmt::Error;
+use std::error::Error;
 
-use redis::{self, ConnectionLike};
+use redis::{Client, Commands, Connection};
+
+use crate::bucket::Bucket;
 
 pub struct RedisClient {
-    _client: redis::cluster::ClusterClient,
-    pub conn: redis::cluster::ClusterConnection,
+    conn: Connection,
 }
 
-#[derive(Debug, Default)]
 impl RedisClient {
-    // Creates a RedisClient by providing him nodes
-    fn new(nodes: Vec<&str>) -> Result<RedisClient, Error> {
-        let client: redis::cluster::ClusterClient = redis::cluster::ClusterClient::open(nodes)
-            .unwrap(
-                "Unable to create a cluster client and open connections to {}",
-                nodes,
-            )?;
-        let conn: redis::cluster::ClusterConnection = client
-            .get_connection()
-            .unwrap("Unable to get connections")?;
-
-        Ok(RedisClient {
-            _client: client,
-            conn,
-        })
+    /// Create a new RedisClient which connects to a given node
+    fn new(addr: &str) -> RedisClient {
+        let client = Client::open(addr).unwrap();
+        let conn = client.get_connection().unwrap();
+        RedisClient { conn }
     }
 
-    /// Creates a RedisClient with redis://127.0.0.1/ as node
-    fn default() -> Result<RedisClient, Error> {
-        RedisClient::new(vec!["redis://127.0.0.1/"])
+    /// Get a bucket using its hash and return it
+    fn get_bucket(&mut self, hash: &str) -> Result<Bucket, Box<dyn Error>> {
+        let stored_data: String = self
+            .conn
+            .get(format!("nova:ratelimiter:buckets:{}", hash))?;
+
+        return Ok(serde_json::from_str(stored_data.as_str())?);
     }
 
-    /// Checks if a bucket is in the Redis store
-    fn has_bucket(&self, hash: &str) -> Result<bool> {
-        let _: () = self.conn.set().unwrap();
-        // todo(n1c00o): continue implementation
+    /// Check if a bucket is in the Redis store
+    fn has_bucket(&mut self, hash: &str) -> Result<bool, Box<dyn Error>> {
+        match self
+            .conn
+            .exists(format!("nova:ratelimiter:buckets:{}", hash))
+        {
+            Err(e) => Err(e.into()),
+            Ok(o) => Ok(o),
+        }
+    }
+
+    /// Create a bucket in the Redis store
+    fn create_bucket(&mut self, hash: &str, bucket: &Bucket) -> Result<(), Box<dyn Error>> {
+        let data: String = serde_json::to_string(&bucket)?;
+        self.conn
+            .set(format!("nova:ratelimiter:buckets:{}", hash), data)?;
+        Ok(())
+    }
+
+    /// Delete a bucket using its hash
+    fn delete_bucket(&mut self, hash: &str) -> Result<(), Box<dyn Error>> {
+        self.conn
+            .del(format!("nova:ratelimiter:buckets:{}", hash))?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::borrow::Borrow;
+
+    use crate::bucket::{Bucket, BucketRequest};
+
+    // Need a Redis to test right now
+    #[test]
+    fn test_redis_client() {
+        let mut client = crate::redis_client::RedisClient::new("redis://127.0.0.1:6379/");
+        let bc = Bucket {
+            limit: 50,
+            remaining: 50,
+            reset: 0,
+            request: BucketRequest {
+                route_name: String::from("/hello_world"),
+                identifiers: vec![String::from("joe"), String::from("mama")],
+            },
+        };
+
+        let hash = "test";
+
+        assert_eq!(client.has_bucket(hash).unwrap(), false);
+
+        match client.create_bucket(hash, &bc) {
+            Err(e) => panic!("{:?}", &e),
+            Ok(_) => {}
+        };
+        assert_eq!(client.has_bucket(hash).unwrap(), true);
+
+        let bck = match client.get_bucket(hash) {
+            Err(e) => panic!("{:?}", &e),
+            Ok(b) => b,
+        };
+        assert_eq!(bck.limit, bc.limit);
+
+        match client.delete_bucket(hash) {
+            Err(e) => panic!("{:?}", &e),
+            Ok(_) => {}
+        };
+        assert_eq!(client.has_bucket(hash).unwrap(), false);
     }
 }
