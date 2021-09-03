6f9f6076b5458d481c328265519a8101b6e42668
Matthieu <matthieu@developershouse.xyz>
2021-09-03 23:55:27 +0400

    fix build

--- ratelimiter/src/main.rs
+++ ratelimiter/src/main.rs
@@ -1,9 +1,5 @@
 // Some implementation of the gRPC service using the shared library.
 
-pub mod pb {
-    tonic::include_proto!("nova.ratelimit.v1.proto");
-}
-
 fn main () {
     println!("Hello world!")
 }
