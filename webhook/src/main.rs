ea06e615db5551ab558f4a3875107a3497ea2b48
n1c00o <git.n1c00o@gmail.com>
2021-09-04 10:41:18 +0200

    Organized imports

--- webhook/src/main.rs
+++ webhook/src/main.rs
@@ -7,20 +7,27 @@ extern crate log;
 pub mod handle;
 pub mod utils;
 
-use utils::{setup_program, Settings};
 use handle::MakeSvc;
+use utils::{setup_program, Settings};
 
 #[tokio::main]
 async fn main() {
     setup_program("webhook");
     let config = Settings::new().unwrap();
-    
+
     let addr = format!("{}:{}", config.server.address, config.server.port)
         .to_socket_addrs()
-        .unwrap().next().unwrap();
+        .unwrap()
+        .next()
+        .unwrap();
 
-    info!("Starting server on {}:{}", config.server.address, config.server.port);
-    let server = Server::bind(&addr).serve(MakeSvc { settings: config.clone() });
+    info!(
+        "Starting server on {}:{}",
+        config.server.address, config.server.port
+    );
+    let server = Server::bind(&addr).serve(MakeSvc {
+        settings: config.clone(),
+    });
 
     if let Err(e) = server.await {
         eprintln!("server error: {}", e);
