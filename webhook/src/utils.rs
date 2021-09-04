78a0d7d42698853e620161f0830ea02b33ee1e32
n1c00o <git.n1c00o@gmail.com>
2021-09-04 10:24:27 +0200

    Fix typo

--- webhook/src/utils.rs
+++ webhook/src/utils.rs
@@ -5,8 +5,8 @@ use log::info;
 use serde::Deserialize;
 
 /// Executes the required configuration steps for the program,
-/// uncluding build information, Sentry and logging.
-pub fn setup_program (_name: &str) {
+/// excluding build information, Sentry and logging.
+pub fn setup_program(_name: &str) {
     pretty_env_logger::init();
 }
 
@@ -22,7 +22,6 @@ pub struct Discord {
     pub client_id: u32,
 }
 
-
 #[derive(Debug, Deserialize, Clone)]
 pub struct Settings {
     pub server: Server,
@@ -35,15 +34,15 @@ impl Settings {
         default.merge(File::with_name("config/default"))?;
         let mode = env::var("ENV").unwrap_or_else(|_| "development".into());
         info!("Configuration Environment: {}", mode);
-        
+
         default.merge(File::with_name(&format!("config/{}", mode)).required(false))?;
         default.merge(File::with_name("config/local").required(false))?;
         default.merge(Environment::with_prefix("NOVA_GATEWAY"))?;
-        
+
         println!("Debug mode: {:?}", default.get_bool("debug"));
 
         let config: Self = default.try_into().unwrap();
 
         Ok(config)
-    }   
+    }
 }
