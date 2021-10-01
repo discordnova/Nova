9b4d9b4feb99a5ec77a608dddec5999326abf8fa
n1c00o <git.n1c00o@gmail.com>
2021-10-01 17:15:47 +0200

    Start doing structs for NATS

--- common/rust/src/payloads.rs
+++ common/rust/src/payloads.rs
@@ -3,13 +3,23 @@ use serde::{Deserialize, Serialize};
 /// Payload send to the nova cache queues
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(bound(deserialize = "T: Deserialize<'de> + std::default::Default + Clone"))]
-pub struct CachePayload<T> {
+pub struct CachePayload {
     pub tracing: Tracing,
-    pub data: T
+    pub data: CacheData,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Tracing {
     pub node_id: String,
-    pub span: Option<String>
+    pub span: Option<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum CacheData {
+    Ready {},
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct ReadyCacheData {
+    pub version: u8,
 }
