e9be1f8b274c90b70551185bd41313d72f2d2294
MatthieuCoder <matthieu@matthieu-dev.xyz>
2023-01-15 19:03:22 +0400

    fix msys2 build

--- exes/gateway/src/main.rs
+++ exes/gateway/src/main.rs
@@ -1,10 +1,10 @@
 use gateway::GatewayServer;
 use leash::ignite;
 
-#[cfg(not(target_env = "msvc"))]
+#[cfg(not(target_os = "windows"))]
 use tikv_jemallocator::Jemalloc;
 
-#[cfg(not(target_env = "msvc"))]
+#[cfg(not(target_os = "windows"))]
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
