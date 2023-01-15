45d25c68dcffa822a5460ea7ee19aad97c855a74
icecodder <git@icecodder.me>
2023-01-15 15:13:32 +0100

    fix typo

--- shell.nix
+++ shell.nix
@@ -1,9 +1,8 @@
 # This defines a function taking `pkgs` as parameter, and uses
 # `nixpkgs` by default if no argument is passed to it.
 { pkgs ? import <nixpkgs> {} }:
-
-# Thisa void typings `pkgs.` before each package name.
-with pkgs;
+	# This avoid typings `pkgs.` before each package name.
+	with pkgs;
 
 # Defines a shell.
 mkShell {
