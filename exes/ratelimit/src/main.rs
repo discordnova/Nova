ea1d345147759a4e2822c4145a87485f10d56a69
Matthieu Pignolet <matthieu@matthieu-dev.xyz>
2023-01-20 16:38:10 +0400

    Draft: New cross building for docker images (#37)
    * Add .circleci/config.yml
    
    * base
    
    * base
    
    * add architectures and cross build
    
    * fix clippy warnings

--- exes/ratelimit/src/main.rs
+++ exes/ratelimit/src/main.rs
@@ -1,11 +1,4 @@
 use leash::ignite;
 use ratelimit::RatelimiterServerComponent;
 
-#[cfg(not(target_os = "windows"))]
-use tikv_jemallocator::Jemalloc;
-
-#[cfg(not(target_os = "windows"))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
-
 ignite!(RatelimiterServerComponent);
--- exes/rest/src/main.rs
+++ exes/rest/src/main.rs
@@ -1,11 +1,4 @@
 use leash::ignite;
 use rest::ReverseProxyServer;
 
-#[cfg(not(target_os = "windows"))]
-use tikv_jemallocator::Jemalloc;
-
-#[cfg(not(target_os = "windows"))]
-#[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
-
 ignite!(ReverseProxyServer);
